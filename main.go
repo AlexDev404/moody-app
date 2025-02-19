b5553577aaa705f271e85fb11b99c3c958140f65
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2025-02-19 18:02:34 +0000

    Add Tailwind CSS configuration and update templates for styling

--- main.go
+++ main.go
@@ -5,30 +5,37 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "./templates/home.html") 
+	http.ServeFile(w, r, "./templates/home.html")
 }
 
-func week1Handler(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "./templates/week1.html")
+func weekHandler(w http.ResponseWriter, r *http.Request) {
+	// Extract the week number from the URL path
+	week := strings.TrimPrefix(r.URL.Path, "/week")
+	if week == "" {
+		http.NotFound(w, r)
+		return
+	}
+	http.ServeFile(w, r, "./templates/week"+week+".html")
 }
 
 func main() {
-        mux := http.NewServeMux()
+	mux := http.NewServeMux()
 
-        // Serve the static files
-        fileServer := http.FileServer(http.Dir("./static"))
-        mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
-        // Register the handlers
-        mux.HandleFunc("/", homeHandler) 
-        mux.HandleFunc("/week1", week1Handler)
-        // Start listening for requests (start the web server)
-        log.Println("Starting server on :4000")
-        err := http.ListenAndServe(":4000", mux)
+	// Serve the static files
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	// Register the handlers
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/week", weekHandler)
+	// Start listening for requests (start the web server)
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
 	// Log error message if server quits unexpectedly
-        if err != nil {
-                log.Fatal(err)
-        }
+	if err != nil {
+		log.Fatal(err)
+	}
 }
