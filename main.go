014d0467602ce19fa253b50d2817fb46efe206b7
alexdev404 <alexdev404@users.noreply.github.com>
2025-02-27 10:07:50 -0600

    Refactor template handling and add new week templates; remove old HTML files

--- main.go
+++ main.go
@@ -3,24 +3,43 @@
 package main
 
 import (
+	"html/template"
 	"log"
+	"log/slog"
 	"net/http"
-	"strings"
+	"os"
 	"time"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./templates/home.html")
+type application struct {
+	logger *slog.Logger
 }
 
-func weekHandler(w http.ResponseWriter, r *http.Request) {
-	// Extract the week number from the URL path
-	week := strings.TrimPrefix(r.URL.Path, "/week")
-	if week == "" {
-		http.NotFound(w, r)
-		return
+type templateData struct {
+	Title    string
+	Greeting string
+}
+
+func (app *application) viewTemplate(w http.ResponseWriter, r *http.Request) {
+	// Get the URL path
+	path := r.URL.Path
+
+	// If the path is the root, serve the index template
+	// Otherwise, serve the template that corresponds to the path
+	if path == "/" {
+		http.ServeFile(w, r, "./templates/index.mustache")
+	} else {
+		// Trim the "/week/" prefix from the path
+		path = path[len("/week/"):]
+		p := templateData{
+			Title:    "Go Study",
+			Greeting: "Hello World Study Mates",
+		}
+
+		// Parse the template file
+		t, _ := template.ParseFiles("./templates/" + "week" + path + ".mustache")
+		t.Execute(w, p)
 	}
-	http.ServeFile(w, r, "./templates/week"+week+".html")
 }
 
 // A basic middleware
@@ -38,13 +57,10 @@ func main() {
 	// Serve the static files
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
-	// Register the handlers
-	mux.HandleFunc("/", homeHandler)
-	mux.HandleFunc("/week1", weekHandler)
-	mux.HandleFunc("/week2", weekHandler)
-	mux.HandleFunc("/week3", weekHandler)
-	mux.HandleFunc("/week4", weekHandler)
-	mux.HandleFunc("/week5", weekHandler)
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	application := &application{logger: logger}
+	// Register the handler
+	mux.HandleFunc("/", application.viewTemplate)
 	// Start listening for requests (start the web server)
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", loggingMiddleware(mux))
